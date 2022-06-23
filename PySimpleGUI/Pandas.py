9ab1976d3a590bf1596de006d01f1237b75bff80
Draca-04 <81396233+Draca-04@users.noreply.github.com>
2022-06-23 18:12:17 +1000

    Working with Pandas
    Getting a basic idea about how Pandas. Using logic discussed between myself and Mr Fong. Code does not currently work nor is useful yet, but using pandas at the moment seems to work better than excel, through it's simple setup.

---
+++ PySimpleGUI/Pandas.py
@@ -0,0 +1,26 @@
+from operator import index
+import pandas as pd
+class Athletics:
+
+    def Athlete(Name, Age): 
+        AthleteID = [0]
+        Athelete_name = pd.DataFrame(
+        {"Athlete" : [str(Name)],
+        "Times" : [11.5, 11.8, 12.9]},
+        index = [1, 2, 3])
+
+    def Times(TimeID, time, AthleteID, EventID):
+        pass 
+         
+
+    def Competition(CompID, Date_Event, Event):
+         Athelete_name = pd.DataFrame(
+        {"Athlete" : [str(CompID)],
+        "Times" : [11.5, 11.8, 12.9]},
+        index = [1, 2, 3])
+
+    def Event(EventID, Event_Type, CompID):
+
+
+
+
