9bb6df17954af9e215c9251e19faf34d7b4eab7d
Draca-04 <81396233+Draca-04@users.noreply.github.com>
2022-06-27 16:33:25 +1000

    Created Athlete Search
    1. Ignore comments
    Created an athlete search that pulls data and variables from Test_Data.py to find an athlete's history.

--- PySimpleGUI/Pandas.py
+++ PySimpleGUI/Pandas.py
@@ -1,26 +1,79 @@
+# Must pip or pip3 install PySimpleGUI in order to work
+# Must pip or pip3 install pandas to work
+
+# imports
 from operator import index
+from sqlite3 import Time
 import pandas as pd
-class Athletics:
+import PySimpleGUI as sg
+import Test_Data as TD
 
-    def Athlete(Name, Age): 
-        AthleteID = [0]
-        Athelete_name = pd.DataFrame(
-        {"Athlete" : [str(Name)],
-        "Times" : [11.5, 11.8, 12.9]},
-        index = [1, 2, 3])
 
-    def Times(TimeID, time, AthleteID, EventID):
-        pass 
+# # Linear Search in Python
+# def Linear_Search(List_Of_Athletes, Length_Of_Array, Search_Athlete):
+
+#     # Going through array sequencially
+#     for i in range(0, Length_Of_Array):
+#         if (List_Of_Athletes[i] == Search_Athlete):
+#             return i
+#     return -1
+
+
+# List_Of_Athletes = ["Joe", "Paul", "Susan", "Aaron", "Charlie", "Bob"]
+# AthleteID = "Bob"
+# Length_Of_Array = len(List_Of_Athletes)
+# result = Linear_Search(List_Of_Athletes, Length_Of_Array, AthleteID)
+# if(result == -1):
+#     print("Athlete not found")
+# else:
+#     print("AthleteID: ", result)
+
+
+
+
+def Athlete(Athlete_Name): 
+
+    a = True
+    for i in range(TD.LenOfAthleteList):
+        if Athlete_Name == TD.AthleteNameList[i]:
+            AthleteID = TD.AthleteProfileList[i]
+        else:
+            print("error")
+            
+
+
+    while a == True:
+        NEW_time = input("Would you like to add a new time? ")
+        if NEW_time == "yes" or NEW_time == "y" or NEW_time == "Yes" or NEW_time == "Y":
+                Time = input("Print athlete's time eg(11.1) ")
+                AddToList = pd.DataFrame(
+                    {"Athlete" : [str(Athlete_Name)],
+                "Times" : [Time]},
+                index = [1])
+
+                AthleteID = AthleteID.append(AddToList, ignore_index=True)
+                print(AthleteID)
+
+        else:
+            print(AthleteID)
+            a = False
+
+
+
+
+    # def Times(TimeID, time, AthleteID, EventID):
+    #     pass 
          
 
-    def Competition(CompID, Date_Event, Event):
-         Athelete_name = pd.DataFrame(
-        {"Athlete" : [str(CompID)],
-        "Times" : [11.5, 11.8, 12.9]},
-        index = [1, 2, 3])
+    # def Competition(CompID, Date_Event, Event):
+    #      Athelete_name = pd.DataFrame(
+    #     {"Athlete" : [str(CompID)],
+    #     "Times" : [11.5, 11.8, 12.9]},
+    #     index = [1, 2, 3])
 
-    def Event(EventID, Event_Type, CompID):
-
-
+    # def Event(EventID, Event_Type, CompID):
 
 
+
+
+print(Athlete("Paul"))
